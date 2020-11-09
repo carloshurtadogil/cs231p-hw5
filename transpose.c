ea58a119e9658eab08f6c27544639726792df85b
Carlos Hurtaado <carlos.hurtado19@yahoo.com>
2020-11-09 13:14:12 -0800

    current progress

--- transpose.c
+++ transpose.c
@@ -1,17 +1,24 @@
 #include <stdio.h> //remove if not using.
-
+#include <pthread.h>
 #include "util.h"//implementing
+
+/* GLOBAL VARIABLES */
+pthread_mutex_t lock;
+unsigned int N, max_row;
+unsigned int batch_size = 0, current_row = 0;
 
 /**
  * Tranpose a given matrix
  * 
  * @param mat Matrix to be transposed
+ * @param start Starting index
+ * @param end End index
 */
-void transpose_mat(Mat *mat) {
+void transpose_mat(Mat *mat, int start, int end) {
     int n = mat->n;
     double temp = -1.0;
-    for (int i = 0; i < n ; i++) {
-        for (int j = i + 1; j < n ; j++) {
+    for (int i = start; i < end; i++) { // rows
+        for (int j = i + 1; j < n ; j++) { // cols
             temp = mat->ptr[i * n + j];
             mat->ptr[i * n + j] = mat->ptr[j * n + i];
             mat->ptr[j * n + i] = temp;
@@ -21,18 +28,82 @@ void transpose_mat(Mat *mat) {
 }
 
 /**
+ * Grab a set of rows that have yet to be calculated
+ * 
+ * @param rows Rows to be calculated
+ * @param size The amount of rows to calculate
+*/
+size_t get_calc_set(unsigned int rows[], size_t size) {
+    size_t i = 0;
+    int start_row;
+    pthread_mutex_lock(&lock);
+
+    if( current_row < max_row ) { // there remain rows unchecked
+        start_row = current_row; // Start with current index
+        current_row += batch_size;
+        pthread_mutex_unlock(&lock);
+        // Get the requested batch size or as many as legal
+        for (i = 0; i < size && start_row <= max_row; ++i) {
+            rows[i] = start_row++;
+        }
+    } else {
+        pthread_mutex_unlock(&lock);
+    }
+
+    return i;
+}
+
+/**
+ * Consumes BATCH_SIZE consecutive rows and calculate the product at each column
+ * 
+ * @param arg The Matrices struct with matrices A, B, and C to be calculated
+*/
+void *consume_and_calculate(void* arg) {
+    Mat *matrix = (Mat *) arg;
+    unsigned int set[batch_size];
+    size_t actual_size = 0;
+
+    // while there are rows left to be processed
+    while ((actual_size = get_calc_set(set, batch_size)) > 0) {
+        for (int i = 0; i < actual_size; ++i) {
+            unsigned int curr_row = set[i];
+            transpose_mat(matrix, curr_row, curr_row + 1);
+        }
+
+        // last batch has been completed
+        if (actual_size < batch_size)
+            break;
+    }
+
+    return NULL;
+}
+
+/**
  * Use a single thread to transpose a matrix
  * 
  * @param mat Matrix to be tranposed
 */
 void mat_sq_trans_st(Mat *mat){
-    transpose_mat(mat);
+    transpose_mat(mat, 0, mat->n);
     return;
 }
 
 void mat_sq_trans_mt(Mat *mat, unsigned int grain, unsigned int threads){
-    //Put your code here.
-    //example_helper_function(2000);
+    batch_size = grain;
+    if (pthread_mutex_init(&lock, NULL) != 0)
+       pthread_exit((void *)1);
+
+    /*Create 'threads' amount of pthreads to carry out tasks*/
+    pthread_t thread_arr[threads];
+    for (int i = 0; i < threads; i++) {
+        if (pthread_create(&thread_arr[i], NULL, &consume_and_calculate, (void *) &mat) != 0)
+            pthread_exit((void *)1);
+    }
+        
+    for (int i = 0; i < threads; ++i)
+        pthread_join(thread_arr[i], NULL);
+
+    pthread_mutex_destroy(&lock);
     return;
 }
 
