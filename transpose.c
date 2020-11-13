867dbcde33cfd37c78d8e290cc6a17b8b0d37bc5
Carlos Hurtaado <carlos.hurtado19@yahoo.com>
2020-11-12 22:57:43 -0800

    documented code

--- transpose.c
+++ transpose.c
@@ -37,6 +37,13 @@ void transpose_mat(Mat *mat, unsigned int start, unsigned int end) {
     }
 }
 
+/**
+ * Tranpose a certain section of a matrix
+ * 
+ * @param mat Matrix to be tranposed
+ * @param i Row index to be transposed
+ * @param j Column index to be transposed
+*/
 void transpose(Mat *mat, unsigned int i, unsigned int j) {
     unsigned int n = mat->n;
     double temp = mat->ptr[i * n + j];
@@ -44,12 +51,19 @@ void transpose(Mat *mat, unsigned int i, unsigned int j) {
     mat->ptr[j * n + i] = temp;
 }
 
+/**
+ * Read in rows and columns to be tranposed
+ * 
+ * @param mat Matrix to be tranposed
+ * @param is Rows to be processed
+ * @param js Columns to be processed
+ * @param len Coarseness for transposition
+*/
 void transpose_part(Mat *mat, unsigned int is[], unsigned int js[], unsigned int len) {
     for (int x = 0; x < len; ++x) {
         transpose(mat, is[x], js[x]);
     }
 }
-
 
 /**
  * Grab a set of rows that have yet to be calculated
@@ -77,8 +91,13 @@ size_t get_calc_set(unsigned int rows[], size_t size) {
     return i;
 }
 
+/**
+ * Retreive a certain amount of indices of the matrix to be transposed
+ * 
+ * @param is Indices representing rows
+ * @param js Indices representing columns
+*/
 int get_indices(unsigned int is[], unsigned int js[]) {
-    int iterations = 0;
     unsigned int cur;
     int i = 0;
     pthread_mutex_lock(&lock);
@@ -112,7 +131,6 @@ void *consume_and_calculate(void *arg) {
     pthread_mutex_unlock(&malloc_lock);
     // while there are rows left to be processed
     while ((actual_size = get_indices(is, js)) > 0) {
-//        printf("Got %d indices", actual_size);
         transpose_part(mat, is, js, c);
         // last batch has been completed
         if (actual_size < c)
@@ -132,11 +150,14 @@ void *consume_and_calculate(void *arg) {
  * @param mat Matrix to be transposed
 */
 void mat_sq_trans_st(Mat *mat) {
-//    printf("\nAddress: %p\n\n", (void*) mat);
     max_row = mat->n;
     transpose_mat(mat, 0, max_row);
 }
 
+/**
+ * Initialize two pointer arrays to represent the necessary rows and columns to transpose
+ * @param n size of arrays
+*/
 void generate_indices(unsigned int n) {
     int count = 0;
     for (int i = 0; i < n; ++i) {
@@ -148,9 +169,14 @@ void generate_indices(unsigned int n) {
     }
 }
 
+/**
+ * Transpose a matrix with respect to multiple threads and different grains
+ * 
+ * @param mat Matrix to be transposed
+ * @param grain Coarseness
+ * @param threads Amount of threads to be utilized
+*/
 void mat_sq_trans_mt(Mat *mat, unsigned int grain, unsigned int threads) {
-//    printf("\nAddress: %p\n\n", (void*) mat);
-    //mat_print(mat);
     i_indices = (unsigned int *) malloc(sizeof(unsigned int) * (mat->n * mat->n - mat->n) / 2);
     j_indices = (unsigned int *) malloc(sizeof(unsigned int) * (mat->n * mat->n - mat->n) / 2);
     generate_indices(mat->n);
@@ -182,28 +208,4 @@ void mat_sq_trans_mt(Mat *mat, unsigned int grain, unsigned int threads) {
     pthread_mutex_destroy(&malloc_lock);
     free(j_indices);
     free(i_indices);
-    //printf("after: \n");
-    //mat_print(mat);
 }
-
-/*static void example_helper_function(int n){
-    // Functions defined with the modifier 'static' are only visible
-    // to other functions in this file. They cannot be called from
-    // outside (for example, from main.c). Use them to organize your
-    // code. Remember that in C, you cannot use a function until you
-    // declare it, so declare all your utility functions above the
-    // point where you use them.
-    //
-    // Maintain the mat_sq_trans_xt functions as lean as possible
-    // because we are measuring their speed. Unless you are debugging,
-    // do not print anything on them, that consumes precious time.
-    //
-    // You may delete this example helper function and structure, and
-    // write your own useful ones.
-
-    Example_Structure es1;
-    es1.example = 13;
-    es1.e_g = 7;
-    printf("n = %d\n", es1.example + es1.e_g + n);
-    return;
-}*/
